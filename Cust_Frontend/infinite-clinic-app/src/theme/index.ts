643515b35213f197ba2e6f4e521f4efc6d29ca08
Aditya <adityaksingh001@gmail.com>
2025-12-28 22:16:03 +0530

    Fix TypeScript unused imports causing Vercel build failure

--- Cust_Frontend/infinite-clinic-app/src/theme/index.ts
+++ Cust_Frontend/infinite-clinic-app/src/theme/index.ts
@@ -1,5 +1,5 @@
-import { background, extendTheme } from '@chakra-ui/react';
-import { color } from 'framer-motion';
+import {  extendTheme } from '@chakra-ui/react';
+
 
 const theme = extendTheme({
   fonts: {
